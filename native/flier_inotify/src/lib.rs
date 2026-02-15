50092120e9e72dddf955b54e853cb1f5f0f5d905
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2026-02-15 21:32:36 +0100

    Remove code comments and debug outputs

--- native/flier_inotify/src/lib.rs
+++ native/flier_inotify/src/lib.rs
@@ -56,122 +56,48 @@ struct WatcherResource {
 #[rustler::resource_impl]
 impl Resource for WatcherResource {
     fn down<'a>(&'a self, _env: Env<'a>, _pid: LocalPid, _mon: Monitor) {
-        // let mut inner = self.inner.lock().unwrap();
-        // assert!(Some(watcher) == inner.watcher);
-        // inner.down_called = true;
-        //
-        println!("cleanup watcher resource\r\n");
         let mut inner = self.inner.lock().unwrap();
         // Stop thread when GC’d
         inner.running.store(false, Ordering::SeqCst);
-        // assert!(Some(watcher) == inner.watcher);
         if let Some(watcher) = inner.watcher.take() {
             let _ = watcher.join();
         }
-        // if let Some(watcher) = self.watcher.lock().unwrap().take() {
-        //     let _ = watcher.join();
-        // }
-        println!("cleaned up watcher resource\r\n");
     }
 }
 
-// impl Drop for WatcherResource {
-//     fn drop(&mut self) {
-//         // Stop thread when GC’d
-//         self.running.store(false, Ordering::SeqCst);
-//         println!("drop1\r\n");
-//         if let Some(watcher) = self.watcher.lock().unwrap().take() {
-//             let _ = watcher.join();
-//         }
-//         println!("drop2\r\n");
-//     }
-// }
-
-// impl Drop for WatcherResource {
-//     fn drop(&mut self) {
-//         println!("drop1\r\n");
-//         let mut inner = self.inner.lock().unwrap();
-//         // Stop thread when GC’d
-//         inner.running.store(false, Ordering::SeqCst);
-//         // assert!(Some(watcher) == inner.watcher);
-//         if let Some(watcher) = inner.watcher.take() {
-//             let _ = watcher.join();
-//         }
-//         println!("drop2\r\n");
-//     }
-// }
-
-// impl Resource for WatcherResourceInner {}
-
-// impl Resource for WatcherResource {}
-
-// #[rustler::nif]
-// // fn on_load(env: Env, _: Term) -> bool {
-// fn on_load(env: Env) -> bool {
-//     println!("ONLOAD");
-//     // let _ = rustler::resource!(WatcherResource, env);
-//     // rustler::resource!(WatcherResource, env) && env.register::<WatcherResource>().is_ok();
-//     env.register::<WatcherResource>().is_ok();
-
-//     true
-// }
-
 #[rustler::nif(schedule = "DirtyIo")]
-// fn start_watcher<'a>(_env: env<'a>, path: string, pid: term<'a>) -> rustler::atom {
-// fn start_watcher<'a>(path: String, pid: Term<'a>) -> Atom {
 fn start_watcher<'a>(
     path: String,
     event_list: Vec<Atom>,
     pid_term: Term<'a>,
 ) -> NifResult<(Atom, ResourceArc<WatcherResource>)> {
-    // let pid = pid.decode::<rustler::types::Pid>().unwrap();
-    // let pid = pid.decode::<rustler::TermType::Pid>().unwrap();
     let (tx, rx) = mpsc::channel();
 
-    println!("start1\r\n");
-    // let pid = pid_term.decode::<LocalPid>().map_err(|_| Error::BadArg)?;
     let pid = pid_term
         .decode::<LocalPid>()
         .map_err(|_| Error::Term(Box::new(no_pid())))?;
 
-    // pid.monitor_process();
-
-    // Ok((ok(), resource))
-    // .map_err(|_| rustler::Error::Term(Box::new(error())))?;
-    println!("start2\r\n");
     let mask = build_watch_mask(&event_list);
-    // let pid = pid_term.decode::<LocalPid>().unwrap();
-    // let path_clone = path.clone();
 
-    println!("start3\r\n");
     // Shared flag to stop the thread if the process dies
     let running = Arc::new(AtomicBool::new(true));
     let running_clone = running.clone();
 
-    println!("start4\r\n");
     let watcher = thread::spawn(move || {
-        println!("start5\r\n");
         let mut owned_env = OwnedEnv::new();
         let mut inotify = match Inotify::init() {
             Ok(i) => i,
             Err(_err) => {
-                // tx.send(Err(format!("init failed: {:?}", err))).unwrap();
                 let _ = tx.send(Err(init_failed()));
 
-                // eprintln!("Failed to initialize inotify: {:?}", err);
-                // return Err(error());
                 return;
             }
         };
 
-        println!("start6\r\n");
         if let Err(_err) = inotify.watches().add(&path, mask) {
             let _ = tx.send(Err(failed_to_add_watcher()));
-            // Err(Error::BadArg);
             return;
-            // return Err(error());
         }
-        println!("start7\r\n");
         let _ = tx.send(Ok(ok()));
 
         let mut buffer = [0; 1024];
@@ -230,23 +156,6 @@ fn start_watcher<'a>(
         }
     });
 
-    // println!("start15\r\n");
-    // match rx.recv() {
-    //     Ok(_) => {
-    //         let resource = ResourceArc::new(WatcherResource {
-    //             inner: Mutex::new(WatcherResourceInner {
-    //                 running,
-    //                 watcher: Some(watcher),
-    //                 down_called: false,
-    //             }),
-    //         });
-
-    //         println!("start16\r\n");
-    //         Ok((ok(), resource))
-    //     }
-    //     Err(err) => Ok(error(), err),
-    // }
-
     match rx.recv() {
         Ok(Ok(_)) => {
             let resource = ResourceArc::new(WatcherResource {
@@ -256,7 +165,6 @@ fn start_watcher<'a>(
                 }),
             });
 
-            println!("start16");
             Ok((ok(), resource))
         }
         Ok(Err(msg)) => {
@@ -273,32 +181,16 @@ fn start_watcher<'a>(
 /// Stop watcher via resource handle
 #[rustler::nif]
 fn stop_watcher(resource: ResourceArc<WatcherResource>) -> Atom {
-    println!("stop1");
     let mut inner = resource.inner.lock().unwrap();
-    println!("stop2");
     // resource.running.store(false, Ordering::SeqCst);
     inner.running.store(false, Ordering::SeqCst);
-    println!("stop3");
 
     if let Some(watcher) = inner.watcher.take() {
-        println!("stop3.5");
         let _ = watcher.join();
     }
-    println!("stop4");
 
     stopped()
 }
-
-// #[rustler::nif]
-// fn on_down(resource: ResourceArc<WatcherResource>, _pid: LocalPid, _reason: Term) {
-//     let mut inner = resource.inner.lock().unwrap();
-
-//     if !inner.down_called {
-//         eprintln!("[on_down] Elixir process died — stopping watcher thread.");
-//         inner.running.store(false, Ordering::SeqCst);
-//         inner.down_called = true;
-//     }
-// }
 
 // rustler::init!("Elixir.Flier.Inotify", [start_watcher]);
 rustler::init!("Elixir.Flier.Inotify.Native");
